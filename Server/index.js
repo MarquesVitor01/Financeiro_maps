fb99af011eb30d645c4e14b59cfcf15cb74f6c11
JuliaFerreiraMelo <juliaferreira.melo@hotmail.com>
2024-11-19 15:58:36 -0300

    geração de boleto funcional

--- Server/index.js
+++ Server/index.js
@@ -8,17 +8,17 @@ const boletoCpf = require("./routes/boletoCpf");
 const app = express();
 const port = process.env.PORT || 5000;
 
+
 app.use(
   cors({
-    origin: "https://grupomapscartaodigital.com.br", 
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true, 
+    origin: "*", 
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
+    allowedHeaders: ["Content-Type", "Authorization"], 
   })
 );
 
 app.options("*", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "https://grupomapscartaodigital.com.br");
+  res.header("Access-Control-Allow-Origin", "*"); 
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.sendStatus(200);
@@ -26,6 +26,7 @@ app.options("*", (req, res) => {
 
 app.use(express.json());
 
+// Rotas
 app.use("/upload", uploadRoutes);
 app.use("/generate-boleto-cnpj", boletoCnpj);
 app.use("/generate-boleto-cpf", boletoCpf);
