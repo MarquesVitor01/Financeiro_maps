0ca7ab4d46e0c8413a4fac6ea3f0e2fbcafb8f36
JuliaFerreiraMelo <juliaferreira.melo@hotmail.com>
2024-11-19 15:19:07 -0300

    geração de boleto funcional 3

--- Server/index.js
+++ Server/index.js
@@ -10,12 +10,21 @@ const port = process.env.PORT || 5000;
 
 app.use(cors({
   origin: "https://grupomapscartaodigital.com.br",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 }));
+
+app.options("*", cors());
+
 app.use(express.json());
 
 app.use("/upload", uploadRoutes);
 app.use("/generate-boleto-cnpj", boletoCnpj);
 app.use("/generate-boleto-cpf", boletoCpf);
+
+app.get("/test-cors", (req, res) => {
+  res.json({ message: "CORS is working!" });
+});
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
